77de522fcb945e5883a80bbc895179849c88e647
DESKTOP-VTNFP7N\user <test@test.com>
2024-07-18 18:50:25 +0900

    07 / 18

--- src/main/java/com/memo/post/PostController.java
+++ src/main/java/com/memo/post/PostController.java
@@ -6,8 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.memo.post.bo.PostBO;
 import com.memo.post.domain.Post;
@@ -50,6 +50,21 @@ public class PostController {
 		return "post/postCreate";
 	}
 	
+	@GetMapping("/post-detail-view")
+	public String postDetailView(
+			@RequestParam("postId") int postId,
+			Model model, HttpSession session) {
+		
+		// db 조회 -userId, postId
+		int userId = (int)session.getAttribute("userId");
+		Post post = postBO.getPostByPostIdUserId(userId, postId);
+		
+		// model에 담기
+		model.addAttribute("post", post);
+		
+		// 화면이동
+		return "post/postDetail";
+	}
 	
 
 }
--- src/main/java/com/memo/post/bo/PostBO.java
+++ src/main/java/com/memo/post/bo/PostBO.java
@@ -26,6 +26,16 @@ public class PostBO {
 		}
 		
 		
+		
+		// input: userId, postId
+		// output: Post or null
+		public Post getPostByPostIdUserId(int userId, int postId) {
+			return postMapper.selectPostByPostIdUserId(userId, postId);
+		}
+		
+		
+		
+		
 		// input: 파라미터들
 		// output: X
 		public void addPost(int userId, String userLoginId, String subject, 
